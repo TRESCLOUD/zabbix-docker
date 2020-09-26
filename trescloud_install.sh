6f2bb974f062bfe6441ce54abb4150b7cea7d73f
Patricio Rangles <patricio.rangles@trescloud.com>
2020-09-25 21:11:54 -0500

    Update trescloud_install.sh
    AGregada construccion de imagen de TRESCLOUD

--- trescloud_install.sh
+++ trescloud_install.sh
@@ -7,5 +7,11 @@
 # el agente de Zabbix, al parecer por un bug en docker-compose no se puede levantar
 # adecuadamente
 
+# construyo la imagen de TRESCLOUD con curl incluido
+docker build -t zabbix-server-pgsql:ubuntu-5.0-latest-trescloud .
+
+# copio el servicio
 sudo cp docker-compose-zabbix.service /etc/systemd/system/
+
+# habilito el servicio
 sudo systemctl enable docker-compose-zabbix

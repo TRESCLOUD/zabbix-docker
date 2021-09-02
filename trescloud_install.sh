b9cfca7e1579155904c7404f8b3aceeb562023a7
Patricio Rangles <patricio.rangles@trescloud.com>
2021-09-01 21:29:54 -0500

    [IMP][Zabbix]: instalacion de postgres
    Postgres se instala directamente en el servidor

--- trescloud_install.sh
+++ trescloud_install.sh
@@ -6,6 +6,24 @@
 # Se descarta el uso de restart: always en el archivo yaml por problemas con 
 # el agente de Zabbix, al parecer por un bug en docker-compose no se puede levantar
 # adecuadamente
+#
+# 2021-09-01: Agregado instalacion de Postgres directo en el servidor para uso de tablas
+#             particionadas y mejora de performance.
+#
+
+#Verifico si Psql de postgres esta instalado
+sudo psql --version
+result=$?
+if [ "${result}" -eq "0" ] ; then
+    echo "`date`: Ya esta instalado postgres"
+else
+    echo "`date`: instalando Postgres 13 ..."
+    sudo apt install wget ca-certificates
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
+    sudo apt update
+    sudo apt-get install -y postgresql-13 postgresql-contrib postgresql-13-partman
+fi
 
 # instalo librerias del sistema operativo y python requeridas
 # por los scripts complemetarios
